9ce8d11ce73c8f77b72076b33956784cbcdc4b77
dudu <dudu@mppdev.(none)>
2014-04-01 10:25:31 +0300

    15 answer

--- 15.rb
+++ 15.rb
@@ -1,1 +1,20 @@
-#Starting in the top left corner of a 2¡Á2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+#Starting in the top left corner of a 2X2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+tmp = Array.new(21){ [0]*21 }
+
+0.upto(20) do |i|
+	0.upto(i) do |j|
+		if j == 0
+			tmp[i][j] = 1
+			tmp[j][i] = tmp[i][j]
+		else
+			tmp[i][j] = tmp[i][j - 1] + tmp[i - 1][j]
+			tmp[j][i] = tmp[i][j]
+		end
+
+	end
+end
+
+puts tmp[20][20]
+
+
