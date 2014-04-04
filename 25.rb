01def35aec3257a42252a1a60360681b06a77ac7
dudu <dudu@mppdev.(none)>
2014-04-04 05:33:49 +0300

    answer

--- 25.rb
+++ 25.rb
@@ -1,2 +1,37 @@
+#The Fibonacci sequence is defined by the recurrence relation:
 #
+#Fn = Fn?1 + Fn?2, where F1 = 1 and F2 = 1.
+#Hence the first 12 terms will be:
 #
+#F1 = 1
+#F2 = 1
+#F3 = 2
+#F4 = 3
+#F5 = 5
+#F6 = 8
+#F7 = 13
+#F8 = 21
+#F9 = 34
+#F10 = 55
+#F11 = 89
+#F12 = 144
+#The 12th term, F12, is the first term to contain three digits.
+#
+#What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+a, b, i= [1,1,1]
+
+loop do
+	i += 1
+	a += b
+	b += a
+	if a.to_s.length == 1000
+		puts i*2 -1
+		break
+	end
+
+	if b.to_s.length == 1000
+		puts i*2
+		break
+	end
+end
--- 26.rb
+++ 26.rb
@@ -1,2 +1,41 @@
+#A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
 #
+#1/2	= 	0.5
+#1/3	= 	0.(3)
+#1/4	= 	0.25
+#1/5	= 	0.2
+#1/6	= 	0.1(6)
+#1/7	= 	0.(142857)
+#1/8	= 	0.125
+#1/9	= 	0.(1)
+#1/10	= 	0.1
+#Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 #
+#Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+
+def cycle x
+	array = []
+	i = 1
+	loop do
+		break if i % x == 0
+		array << i
+		i = (i % x) * 10
+		if ((array.include? i) && (i / x != 0))
+			return array.length - array.index(i)
+		end
+	end
+	return 0
+end
+
+d = 1
+md = 0
+2.upto(999) do |x|
+	mx = cycle(x)
+	if mx > md
+		md = mx 
+		d = x
+	end
+end
+
+puts d
